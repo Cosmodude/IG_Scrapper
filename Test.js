9a75764e9501671ffdaf36cefcbb7c71efe84687
Hackdady <lenskii.vm19@physics.msu.ru>
2023-06-12 21:37:48 +0900

    added package

--- Test.js
+++ Test.js
@@ -7,5 +7,5 @@ console.log(pattern.test(b));
 const ab = [1, 2, 3, 4, 5, 6];
 const bc = [6, 9, 7, 4]
 
-const c = bc.filter(i => ab.indexOf(i)>=0);
+const c = bc.filter(i => ab.find(i));
 console.log(c);
